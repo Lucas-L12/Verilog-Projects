ae9f150a9190d5d893742427561f33b115efb4e0
Lucas-L12 <lopezzlucas12@gmail.com>
2026-04-25 20:33:53 +0200

    Update baud_rate_generator.v

--- UART/src/baud_rate_generator.v
+++ UART/src/baud_rate_generator.v
@@ -1,1 +1,32 @@
-Baudrate
+`timescale 1ns / 1ps
+
+module baud_rate_generator
+#(
+    parameter M=27,//baud rate divisor
+              N=8  
+)
+(   
+    input wire clk,reset,
+    output wire max_tick,
+    output wire [N-1:0] q
+     );
+     
+     //signal declaration
+     reg [N-1:0] r_reg;
+     wire [N-1:0] r_next;
+     //register
+     always @(posedge clk, posedge reset)
+        if(reset)
+            r_reg<=0;
+        else 
+            r_reg<=r_next;
+     //next state logic
+     assign r_next=(r_reg==M-1)? 0: r_reg+1;
+     
+     //output logic
+     assign max_tick=(r_reg==M-1);
+     assign q= r_reg;
+     
+     
+endmodule
+
